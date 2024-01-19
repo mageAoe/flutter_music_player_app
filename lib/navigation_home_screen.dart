0bc012a87cfbe623843cd575d5b936c02414cc3e
sunchaoqun <scq1144282310@163.com>
2024-01-19 18:08:57 +0800

    update: Navigation

--- lib/navigation_home_screen.dart
+++ lib/navigation_home_screen.dart
@@ -2,6 +2,10 @@ import 'package:flutter/material.dart';
 import './app_theme.dart';
 import './home_screen.dart';
 import 'custom_drawer/drawer_user_controller.dart';
+import 'package:flutter_music_player_app/utlis/enum_setting.dart';
+import 'package:flutter_music_player_app/pages/help_screen_view/help_screen_view.dart';
+import 'package:flutter_music_player_app/pages/feedback_screen_view/feedback_screen_view.dart';
+import 'package:flutter_music_player_app/pages/Invite_friend_view/Invite_friend_view.dart';
 
 class NavigationHomeScreen extends StatefulWidget {
   const NavigationHomeScreen({super.key});
@@ -11,27 +15,68 @@ class NavigationHomeScreen extends StatefulWidget {
 }
 
 class _NavigationHomeScreenState extends State<NavigationHomeScreen> {
+  Widget? screenView;
+   DrawerIndex? drawerIndex;
+
+   @override
+  void initState() {
+    drawerIndex = DrawerIndex.HOME;
+    screenView =  const MyHomePage(title: '网易云音乐');
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
       color: AppTheme.white,
-      child: const SafeArea(
+      child: SafeArea(
         top: false,
         bottom: false,
         child: Scaffold(
           backgroundColor: AppTheme.nearlyWhite,
           body: DrawerUserController(
-            // screenIndex: drawerIndex,
-            // drawerWidth: MediaQuery.of(context).size.width * 0.75,
-            // onDrawerCall: (DrawerIndex drawerIndexdata) {
-            //   changeIndex(drawerIndexdata);
-            //   //callback from drawer for replace screen as user need with passing DrawerIndex(Enum index)
-            // },
-            screenView: MyHomePage(title: 'my flutter music player'),
+            screenIndex: drawerIndex,
+            drawerWidth: MediaQuery.of(context).size.width * 0.75,
+            onDrawerCall: (DrawerIndex drawerIndexdata) {
+              changeIndex(drawerIndexdata);
+              //callback from drawer for replace screen as user need with passing DrawerIndex(Enum index)
+            },
+            screenView: screenView,
             //we replace screen view as we need on navigate starting screens like MyHomePage, HelpScreen, FeedbackScreen, etc...
           ),
         ),
       ),
     );
+  }
+
+
+  void changeIndex(DrawerIndex drawerIndexdata) {
+    if (drawerIndex != drawerIndexdata) {
+      drawerIndex = drawerIndexdata;
+      switch (drawerIndex) {
+        case DrawerIndex.HOME:
+          setState(() {
+            screenView = const MyHomePage(title: '网易云音乐');
+          });
+          break;
+        case DrawerIndex.Help:
+          setState(() {
+            screenView = const HelpScreen();
+          });
+          break;
+        case DrawerIndex.FeedBack:
+          setState(() {
+            screenView = const FeedbackScreen();
+          });
+          break;
+        case DrawerIndex.Invite:
+          setState(() {
+            screenView = const InviteFriend();
+          });
+          break;
+        default:
+          break;
+      }
+    }
   }
 }
