81576f29948ec472cc7c364f2c0895ade3438ef7
sunchaoqun <scq1144282310@163.com>
2024-02-28 17:58:43 +0800

    update： 歌单页面

--- lib/views/song_sheet/song_sheet_view.dart
+++ lib/views/song_sheet/song_sheet_view.dart
@@ -1,6 +1,6 @@
 import 'dart:ui';
 import 'package:flutter/material.dart';
-import 'package:flutter_music_player_app/app_theme.dart';
+import 'package:flutter_music_player_app/theme/app_theme.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class SongSheetView extends StatefulWidget {
