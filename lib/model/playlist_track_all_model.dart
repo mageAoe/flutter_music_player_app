0db8d854dd814bc55ae92b356a71a55f388a103b
sunchaoqun <scq1144282310@163.com>
2024-03-06 15:04:36 +0800

    update： 歌单详情

---
+++ lib/model/playlist_track_all_model.dart
@@ -0,0 +1,333 @@
+class PlaylistTrackAllModel {
+  PlaylistTrackAllModel({
+    this.songs,
+    this.code,
+  });
+  List<Songs>? songs;
+  int? code;
+  
+  PlaylistTrackAllModel.fromJson(Map<String, dynamic> json){
+    songs = List.from(json['songs']).map((e)=>Songs.fromJson(e)).toList();
+    code = json['code'];
+  }
+}
+
+class Songs {
+  Songs({
+    this.name,
+    this.id,
+    this.pst,
+    this.t,
+    this.ar,
+    this.alia,
+    this.pop,
+    this.st,
+    this.rt,
+    this.fee,
+    this.v,
+    this.cf,
+    this.al,
+    this.dt,
+    // this.h,
+    this.m,
+    this.l,
+    // this.sq,
+    this.cd,
+    this.no,
+    this.ftype,
+    this.rtUrls,
+    this.djId,
+    this.copyright,
+    this.sId,
+    this.mark,
+    this.originCoverType,
+    this.resourceState,
+    this.version,
+    this.single,
+    this.mv,
+    this.mst,
+    this.cp,
+    this.rtype,
+    this.publishTime,
+    this.tns,
+  });
+  String? name;
+  int? id;
+  int? pst;
+  int? t;
+  List<Ar>? ar;
+  List<String>? alia;
+  int? pop;
+  int? st;
+  String? rt;
+  int? fee;
+  int? v;
+  String? cf;
+  Al? al;
+  int? dt;
+  // H? h;
+  M? m;
+  L? l;
+  // Sq? sq;
+  String? cd;
+  int? no;
+  int? ftype;
+  List<dynamic>? rtUrls;
+  int? djId;
+  int? copyright;
+  int? sId;
+  int? mark;
+  int? originCoverType;
+  bool? resourceState;
+  int? version;
+  int? single;
+  int? mv;
+  int? mst;
+  int? cp;
+  int? rtype;
+  int? publishTime;
+  List<String>? tns;
+  
+  Songs.fromJson(Map<String, dynamic> json){
+    name = json['name'];
+    id = json['id'];
+    pst = json['pst'];
+    t = json['t'];
+    ar = List.from(json['ar']).map((e)=>Ar.fromJson(e)).toList();
+    alia = List.castFrom<dynamic, String>(json['alia']);
+    pop = json['pop'];
+    st = json['st'];
+    rt = json['rt'];
+    fee = json['fee'];
+    v = json['v'];
+    cf = json['cf'];
+    al = Al.fromJson(json['al']);
+    dt = json['dt'];
+    // h = H.fromJson(json['h']);
+    m = M.fromJson(json['m']);
+    l = L.fromJson(json['l']);
+    // sq = Sq.fromJson(json['sq']);
+    cd = json['cd'];
+    no = json['no'];
+    ftype = json['ftype'];
+    rtUrls = List.castFrom<dynamic, dynamic>(json['rtUrls']);
+    djId = json['djId'];
+    copyright = json['copyright'];
+    sId = json['s_id'];
+    mark = json['mark'];
+    originCoverType = json['originCoverType'];
+    resourceState = json['resourceState'];
+    version = json['version'];
+    single = json['single'];
+    mv = json['mv'];
+    mst = json['mst'];
+    cp = json['cp'];
+    rtype = json['rtype'];
+    publishTime = json['publishTime'];
+    // tns = List.castFrom<dynamic, String>(json['tns']);
+    if (json['tns'] != null) {
+      tns = <String>[];
+      json['tns'].forEach((v) {
+        tns!.add(v);
+      });
+    }
+  }
+}
+
+class Ar {
+  Ar({
+    this.id,
+    this.name,
+    this.tns,
+    this.alias,
+  });
+  int? id;
+  String? name;
+  List<dynamic>? tns;
+  List<dynamic>? alias;
+  
+  Ar.fromJson(Map<String, dynamic> json){
+    id = json['id'];
+    name = json['name'];
+    tns = List.castFrom<dynamic, dynamic>(json['tns']);
+    alias = List.castFrom<dynamic, dynamic>(json['alias']);
+  }
+
+  Map<String, dynamic> toJson() {
+    final data = <String, dynamic>{};
+    data['id'] = id;
+    data['name'] = name;
+    data['tns'] = tns;
+    data['alias'] = alias;
+    return data;
+  }
+}
+
+class Al {
+  Al({
+    this.id,
+    this.name,
+    this.picUrl,
+    this.tns,
+    this.picStr,
+    this.pic,
+  });
+  int? id;
+  String? name;
+  String? picUrl;
+  List<dynamic>? tns;
+  String? picStr;
+  int? pic;
+  
+  Al.fromJson(Map<String, dynamic> json){
+    id = json['id'];
+    name = json['name'];
+    picUrl = json['picUrl'];
+    tns = List.castFrom<dynamic, dynamic>(json['tns']);
+    picStr = json['pic_str'];
+    pic = json['pic'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final data = <String, dynamic>{};
+    data['id'] = id;
+    data['name'] = name;
+    data['picUrl'] = picUrl;
+    data['tns'] = tns;
+    data['pic_str'] = picStr;
+    data['pic'] = pic;
+    return data;
+  }
+}
+
+class H {
+  H({
+    this.br,
+    this.fid,
+    this.size,
+    this.vd,
+    this.sr,
+  });
+  int? br;
+  int? fid;
+  int? size;
+  int? vd;
+  int? sr;
+  
+  H.fromJson(Map<String, dynamic> json){
+    br = json['br'];
+    fid = json['fid'];
+    size = json['size'];
+    vd = json['vd'];
+    sr = json['sr'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final data = <String, dynamic>{};
+    data['br'] = br;
+    data['fid'] = fid;
+    data['size'] = size;
+    data['vd'] = vd;
+    data['sr'] = sr;
+    return data;
+  }
+}
+
+class M {
+  M({
+    this.br,
+    this.fid,
+    this.size,
+    this.vd,
+    this.sr,
+  });
+  int? br;
+  int? fid;
+  int? size;
+  int? vd;
+  int? sr;
+  
+  M.fromJson(Map<String, dynamic> json){
+    br = json['br'];
+    fid = json['fid'];
+    size = json['size'];
+    vd = json['vd'];
+    sr = json['sr'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final data = <String, dynamic>{};
+    data['br'] = br;
+    data['fid'] = fid;
+    data['size'] = size;
+    data['vd'] = vd;
+    data['sr'] = sr;
+    return data;
+  }
+}
+
+class L {
+  L({
+    this.br,
+    this.fid,
+    this.size,
+    this.vd,
+    this.sr,
+  });
+  int? br;
+  int? fid;
+  int? size;
+  int? vd;
+  int? sr;
+  
+  L.fromJson(Map<String, dynamic> json){
+    br = json['br'];
+    fid = json['fid'];
+    size = json['size'];
+    vd = json['vd'];
+    sr = json['sr'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final data = <String, dynamic>{};
+    data['br'] = br;
+    data['fid'] = fid;
+    data['size'] = size;
+    data['vd'] = vd;
+    data['sr'] = sr;
+    return data;
+  }
+}
+
+class Sq {
+  Sq({
+    this.br,
+    this.fid,
+    this.size,
+    this.vd,
+    this.sr,
+  });
+  int? br;
+  int? fid;
+  int? size;
+  int? vd;
+  int? sr;
+  
+  Sq.fromJson(Map<String, dynamic> json){
+    br = json['br'];
+    fid = json['fid'];
+    size = json['size'];
+    vd = json['vd'];
+    sr = json['sr'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final data = <String, dynamic>{};
+    data['br'] = br;
+    data['fid'] = fid;
+    data['size'] = size;
+    data['vd'] = vd;
+    data['sr'] = sr;
+    return data;
+  }
+}
---
+++ lib/views/song_sheet/song_detail_view.dart
@@ -0,0 +1,290 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_music_player_app/theme/app_theme.dart';
+import 'package:flutter_music_player_app/theme/public_style.dart';
+import 'package:flutter_music_player_app/utlis/fonts.dart';
+import 'package:flutter_music_player_app/api/my_api.dart';
+import 'package:flutter_music_player_app/model/playlist_track_all_model.dart';
+
+
+class SongDetailView extends StatefulWidget {
+  final int id;
+  final String coverImgUrl;
+  final String name;
+  final String? nickname;
+  final String? avatar;
+
+  const SongDetailView({
+    super.key, 
+    required this.id, 
+    required this.coverImgUrl,
+    required this.name,
+    required this.nickname,
+    required this.avatar
+  });
+
+  @override
+  State<SongDetailView> createState() => _SongDetailViewState();
+}
+
+class _SongDetailViewState extends State<SongDetailView> {
+  
+  PlaylistTrackAllModel userPlaylistData = PlaylistTrackAllModel();
+  int offset = 0;
+  int limit = 20;
+  double safeAreaTop = 0.0;
+  ScrollController scrollController = ScrollController();
+
+  List btnList = [
+    {'name': '分享', 'icon': Icons.share },
+    {'name': '评论', 'icon': YunMusicFont.message },
+    {'name': '收藏', 'icon': Icons.collections_bookmark_outlined },
+  ];
+
+  // 获取歌单里面的歌曲
+  checkLoginStatus() async {
+    MyApi.getPlaylistTrackAll('id=${widget.id}&limit=$limit&offset=$offset').then((playlist){
+      if(playlist != null){
+        print('playlist: $playlist');
+          setState(() {
+            userPlaylistData = playlist;
+          });
+        }
+    });
+  }
+
+  String getTns(List<String>? tns){
+    if(tns != null){
+      return '(${tns[0]})';
+    }else{
+      return '';
+    }
+  }
+
+
+  @override
+  void initState() {
+    print('widget.id:  ${widget.id}');
+    checkLoginStatus();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // 获取屏幕信息
+    MediaQueryData mediaQueryData = MediaQuery.of(context);
+    // 获取刘海屏的高度
+    safeAreaTop = mediaQueryData.padding.top;
+
+    return Scaffold(
+      extendBodyBehindAppBar: true, // 扩展body区域到AppBar
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(120.h),
+        child: AppBar(
+          backgroundColor:  Colors.white.withOpacity(0.0),
+          elevation: 0,
+          leading: IconButton(onPressed: (){
+            Navigator.pop(context);
+          }, icon: const Icon(Icons.arrow_back,color: AppTheme.allWhite)),
+          title: Text('歌单', style: TextStyle(fontSize: 45.sp, fontWeight: FontWeight.bold)),
+          actions: [
+              IconButton(onPressed: (){}, icon: const Icon(Icons.search,color: AppTheme.allWhite)),
+              IconButton(onPressed: (){}, icon: const Icon(Icons.more_vert_outlined,color: AppTheme.allWhite))
+          ],
+        ),
+      ),
+      body: SingleChildScrollView(
+        controller: scrollController,
+        child: Column(
+          children: [
+            Container(
+              width: double.infinity,
+              height: 890.h,
+              padding: EdgeInsets.fromLTRB(40.w, safeAreaTop + 120.h , 40.w, 40.w),
+              decoration: const BoxDecoration(
+                gradient: songDetailHeadLinearGradientStyle
+              ),
+              child: Column(
+                children: [
+                  SizedBox(height: 40.h),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(
+                        width: 330.w,
+                        height: 330.w,
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(15),
+                          child: Image.network(widget.coverImgUrl, fit: BoxFit.cover)
+                        ),
+                      ),
+                      SizedBox(width: 30.h),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          SizedBox(
+                            width: MediaQuery.of(context).size.width - 480.w, // 80 + 30 + 330 = 480
+                            child: Text(
+                              widget.name, 
+                              softWrap: true,
+                              style: TextStyle(fontSize: 45.sp, fontWeight: FontWeight.bold, color: AppTheme.allWhite)
+                          ),
+                          ),
+                          
+                          SizedBox(height: 30.h),
+                          Row(
+                            children: [
+                              SizedBox(
+                                width: 80.w,
+                                height: 80.w,
+                                child: ClipRRect(
+                                  borderRadius: BorderRadius.circular(40),
+                                  child: Image.network("${widget.avatar}", fit: BoxFit.cover)
+                                ),
+                              ),
+                              SizedBox(width: 30.h),
+                              Row(
+                                children: [
+                                  Text(widget.nickname!, style: TextStyle(fontSize: 35.sp, color: AppTheme.songDeatilSamllSize)),
+                                  Icon(Icons.arrow_forward_ios_outlined, size: 35.sp, color: AppTheme.songDeatilSamllSize)
+                                ],
+                              )
+                            ],
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                  SizedBox(height: 40.h),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: btnList.map((e){
+                      return ClipRRect(
+                        borderRadius: BorderRadius.circular(45),
+                        child: Container(
+                          padding: EdgeInsets.fromLTRB(60.w, 20.w, 60.w, 20.w),
+                          decoration: BoxDecoration(
+                            color: AppTheme.songDeatilBtnBg,
+                            borderRadius: BorderRadius.circular(20)
+                          ),
+                          child: Row(
+                            children: [
+                              Icon(e['icon'], color: AppTheme.allWhite),
+                              SizedBox(width: 20.w),
+                              Text('${e['name']}', style: const TextStyle(color: AppTheme.allWhite, fontWeight: FontWeight.w700))
+                            ],
+                          ),
+                        )
+                      );
+                    }).toList()
+                  )
+                ],
+              ),
+            ),
+            Transform.translate(
+              offset: const Offset(0.0, -30.0),
+              child: Container(
+                // width: double.infinity,
+                padding: EdgeInsets.only(top: 40.h, bottom: 120.h),
+                decoration: const BoxDecoration(
+                  color: AppTheme.myBg,
+                  borderRadius: BorderRadius.vertical(top: Radius.circular(30.0)),
+                ),
+                child: Column(
+                  children: [
+                    ListTile(
+                      leading: const Icon(Icons.play_circle_rounded, color: AppTheme.primary),
+                      title: Row(
+                        children: [
+                          const Text('播放全部'),
+                          Text('${userPlaylistData.songs?.length}首')
+                        ],
+                      ),
+                      trailing: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          const Icon(YunMusicFont.yinle, color: Colors.black),
+                          SizedBox(width: 40.w),
+                          const Icon(Icons.download, color: Colors.black),
+                          SizedBox(width: 40.w),
+                          const Icon(Icons.menu_open_sharp, color: Colors.black)
+                        ],
+                      ),
+                    ),
+                    if (userPlaylistData.songs != null && userPlaylistData.songs!.isNotEmpty)
+                      ...List.generate(
+                        userPlaylistData.songs!.length,
+                        (index) => ListTile(
+                          leading: Text('${index + 1}'), // 递增的数字
+                          title: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Row(
+                                textDirection: TextDirection.ltr,
+                                textBaseline: TextBaseline.alphabetic,
+                                mainAxisSize: MainAxisSize.min,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Expanded(
+                                    child: Text(
+                                      '${userPlaylistData.songs![index].name} ${getTns(userPlaylistData.songs![index].tns)}', 
+                                      overflow: TextOverflow.ellipsis, maxLines: 1
+                                    )
+                                  ),
+                                ],
+                              ),
+                              SizedBox(height: 15.h),
+                              Row(
+                                textBaseline: TextBaseline.alphabetic,
+                                mainAxisSize: MainAxisSize.min,
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: [
+                                  Icon(YunMusicFont.love, size: 32.w, color: AppTheme.primary),
+                                  SizedBox(width: 15.w),
+                                  ...List.generate(
+                                    userPlaylistData.songs![index].ar!.length,
+                                    (arIndex) {
+                                      if(arIndex == userPlaylistData.songs![index].ar!.length - 1){
+                                        return Text('${userPlaylistData.songs![index].ar![arIndex].name}', style: TextStyle(fontSize: 36.sp,color: AppTheme.myLoveSingSubtext));
+                                      }else{
+                                        return Text('${userPlaylistData.songs![index].ar![arIndex].name} / ', style: TextStyle(fontSize: 36.sp,color: AppTheme.myLoveSingSubtext));
+                                      }
+                                    }
+                                  ),
+                                  Padding(
+                                    padding: EdgeInsets.symmetric(horizontal: 15.w),
+                                    child: Text('-', style: TextStyle(fontSize: 36.sp))
+                                  ),
+                                  Expanded(
+                                    child: Text(
+                                      '${userPlaylistData.songs![index].al!.name}', 
+                                      style: TextStyle(fontSize: 36.sp,color: AppTheme.myLoveSingSubtext), overflow: TextOverflow.ellipsis)
+                                  )
+                                ],
+                              )
+                            ],
+                          ),
+                          trailing: const Icon(Icons.more_vert_outlined),
+                        ),
+                      )
+                    else
+                      Container(
+                        margin: EdgeInsets.only(top: 120.h),
+                        child: Center(
+                          child: Text(
+                            '暂无歌曲',
+                            style: TextStyle(fontSize: 44.sp, fontWeight: FontWeight.bold, color: AppTheme.myLoveSingText),
+                          ),
+                        ),
+                      ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        )
+      ),
+    );
+  }
+}
