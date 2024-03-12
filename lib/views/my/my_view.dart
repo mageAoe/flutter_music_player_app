c328d9c6e1c1803923784c59e3747eb7507db8f3
sunchaoqun <scq1144282310@163.com>
2024-03-12 09:18:18 +0800

    feat: 提取我的页面组件、新增歌单详情

--- lib/views/my/my_view.dart
+++ lib/views/my/my_view.dart
@@ -6,12 +6,10 @@ import 'package:flutter_music_player_app/theme/app_theme.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_music_player_app/utlis/fonts.dart';
 import 'package:flutter_music_player_app/widgets/my_widget.dart';
-import 'package:flutter_music_player_app/views/login/login.dart';
 import 'package:flutter_music_player_app/api/login_api.dart';
 import 'package:flutter_music_player_app/model/login_model.dart';
 import 'package:flutter_music_player_app/model/user_detail_model.dart';
 import 'package:flutter_music_player_app/api/my_api.dart';
-import 'package:flutter_music_player_app/views/song_sheet/song_detail_view.dart';
 import 'package:flutter_music_player_app/services/provider.dart';
 import 'package:provider/provider.dart';
 import 'package:skeletonizer/skeletonizer.dart';
@@ -53,7 +51,9 @@ class _MyViewState extends State<MyView> with SingleTickerProviderStateMixin {
           userInfoData = userInfo;
           getUserInfo();
           getUserPlaylist();
-        }
+      }else{
+        setIsMyLoading(false);
+      }
     });
   }
 
@@ -138,7 +138,6 @@ class _MyViewState extends State<MyView> with SingleTickerProviderStateMixin {
         preferredSize: Size.fromHeight(120.h),
         child: AppBar(
           centerTitle: true,
-          // backgroundColor: Colors.white.withOpacity(0.0),
           backgroundColor: showSubHeaderTabs?  const Color.fromARGB(255, 214, 201, 201) : Colors.white.withOpacity(0.0),
           elevation: 0,
           leading: const Icon(Icons.menu,color: AppTheme.allWhite),
@@ -203,252 +202,6 @@ class _MyViewState extends State<MyView> with SingleTickerProviderStateMixin {
           ],
         ),
       )
-    );
-  }
-}
-
-// 登录后展示歌单
-class MyLoginBottomWidget extends StatelessWidget {
-  const MyLoginBottomWidget({
-    super.key,
-    required this.userPlaylistData,
-    required this.userDetailData
-  });
-
-  final UserDetailModel userDetailData;
-  final UserPlaylistModel userPlaylistData;
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      // shrinkWrap: true,
-      // padding: EdgeInsets.zero,
-      children:  [
-        ListTile(
-          title: Text('音乐', style: TextStyle(fontSize: 48.sp, fontWeight: FontWeight.bold)),
-        ),
-        ListTile(
-          title: Text('近期', style:  TextStyle(fontSize: 40.sp, fontWeight: FontWeight.bold)),
-        ),
-        ...userPlaylistData.playlist!.map((e) {
-          return Padding(
-            padding: EdgeInsets.only(top: 20.h, bottom: 20.h),
-            child: InkWell(
-              onTap: (){
-                Navigator.push( 
-                  context,
-                  MaterialPageRoute(builder: (context) {
-                    return SongDetailView(
-                      id: e.id!,
-                      coverImgUrl: e.coverImgUrl!,
-                      name: e.name!,
-                      nickname: e.creator?.nickname,
-                      avatar: userDetailData.profile?.avatarUrl
-                    );
-                  }),
-                );
-              },
-              child: ListTile(
-                leading: ClipRRect(
-                  borderRadius: BorderRadius.circular(12),
-                  child: Image.network("${e.coverImgUrl}")
-                ),
-                title: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      '${e.name}', 
-                      overflow: TextOverflow.ellipsis,
-                      style:  TextStyle(fontSize: 44.sp, fontWeight: FontWeight.bold, color: AppTheme.myLoveSingText)
-                    ),
-                    SizedBox(height: 15.h),
-                    Row(
-                      children: [
-                        Text('歌单 ·', style: TextStyle(color: AppTheme.myLoveSingSubtext, fontSize: 34.sp)),
-                        Text('${e.trackCount}首 ·', style: TextStyle(color: AppTheme.myLoveSingSubtext, fontSize: 34.sp)),
-                        Text('${e.creator?.nickname}', style: TextStyle(color: AppTheme.myLoveSingSubtext, fontSize: 34.sp)),
-                      ],
-                    )
-                  ],
-                ),
-              ),
-            ),
-          );
-        }).toList(),
-      ],
-    );
-  }
-}
-
-// 登录后的顶部
-class MyLoginTopWidget extends StatelessWidget {
-  const MyLoginTopWidget({
-    super.key,
-    required this.userDetailData,
-    required this.iconsList,
-  });
-
-  final UserDetailModel userDetailData;
-  final List iconsList;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 1200.h,
-      padding: EdgeInsets.fromLTRB(40.w, 240.w, 40.w, 40.w),
-      decoration: BoxDecoration(
-        image: DecorationImage(
-          fit: BoxFit.cover,
-          opacity: 0.8,
-          colorFilter: const ColorFilter.mode(Colors.black, BlendMode.colorDodge), // color
-          image: NetworkImage('${userDetailData.profile?.backgroundUrl}')
-        )
-      ),
-      child: Column(
-        children: [
-          // const TopNavBarWidget(),
-          SizedBox(height: 40.h),
-          MyAvatarWidget(avatar: userDetailData.profile?.avatarUrl),
-          SizedBox(height: 40.h),
-          Text('${userDetailData.profile?.nickname }', style: TextStyle(fontSize: 62.sp, color: AppTheme.allWhite, fontWeight: FontWeight.bold)),
-          SizedBox(height: 20.h),
-          Text('${userDetailData.profile?.signature}', style: TextStyle(fontSize: 34.sp, color: const Color.fromARGB(255, 228, 225, 225), fontWeight: FontWeight.bold)),
-          SizedBox(height: 30.h),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Row(
-                children: [
-                  Text('${userDetailData.profile?.newFollows}', style: TextStyle(fontSize: 48.sp, color: AppTheme.allWhite, fontWeight: FontWeight.bold)),
-                  SizedBox(width: 20.w),
-                  Text('关注', style: TextStyle(fontSize: 34.sp, color: const Color.fromARGB(255, 228, 225, 225), fontWeight: FontWeight.bold)),
-                ],
-              ),
-              Row(
-                children: [
-                  Text('${userDetailData.profile?.followeds}', style: TextStyle(fontSize: 48.sp, color: AppTheme.allWhite, fontWeight: FontWeight.bold)),
-                  SizedBox(width: 20.w),
-                  Text('粉丝', style: TextStyle(fontSize: 34.sp, color: const Color.fromARGB(255, 228, 225, 225), fontWeight: FontWeight.bold)),
-                ],
-              ),
-              Row(
-                children: [
-                  Text('Lv.${userDetailData.level}', style: TextStyle(fontSize: 48.sp, color: AppTheme.allWhite, fontWeight: FontWeight.bold)),
-                  SizedBox(width: 20.w),
-                  Text('等级', style: TextStyle(fontSize: 34.sp, color: const Color.fromARGB(255, 228, 225, 225), fontWeight: FontWeight.bold)),
-                ],
-              ),
-            ],
-          ),
-          SizedBox(height: 30.h),
-          MyActionsWidget(iconsList: iconsList)
-        ],
-      ),
-    );
-  }
-}
-
-// 顶部自定义导航
-class TopNavBarWidget extends StatelessWidget {
-  const TopNavBarWidget({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        const Icon(Icons.menu,color: AppTheme.allWhite),
-        SizedBox(
-          child: Row(
-            children: [
-              IconButton(onPressed: (){}, icon: const Icon(Icons.search,color: AppTheme.allWhite)),
-              IconButton(onPressed: (){}, icon: const Icon(Icons.more_vert_outlined,color: AppTheme.allWhite))
-            ],
-          ),
-        )
-      ],
-    );
-  }
-}
-
-// 未登录时底下部分
-class MyNotLoginBottomWidget extends StatelessWidget {
-  const MyNotLoginBottomWidget({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      // padding: EdgeInsets.zero,
-      children: [
-        Text('音乐', style: TextStyle(fontSize: 48.sp, fontWeight: FontWeight.bold)),
-        SizedBox(height: 30.h),
-        Text('近期', style: TextStyle(fontSize: 40.sp, fontWeight: FontWeight.bold)),
-        SizedBox(height: 30.h),
-        Row(
-          children: [
-            Container(
-              width: 180.w,
-              height: 180.w,
-              decoration: BoxDecoration(
-                color: AppTheme.myLoveSing,
-                borderRadius: BorderRadius.circular(8)
-              ),
-              child: const Icon(Icons.heart_broken, color: AppTheme.allWhite, size: 48),
-            ),
-            SizedBox(width: 20.w),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text('我喜欢的音乐', style: TextStyle(fontSize: 40.sp, fontWeight: FontWeight.bold, color: const Color.fromRGBO(67,70,84,1))),
-                Text('0首', style: TextStyle(fontSize: 35.sp, fontWeight: FontWeight.bold, color: const Color.fromRGBO(135,137,147,1)))
-              ],
-            )
-          ],
-        )
-      ],
-    );
-  }
-}
-
-// 未登录时上面部分
-class MyNotLoginTopWidget extends StatelessWidget {
-  const MyNotLoginTopWidget({
-    super.key,
-    required this.iconsList,
-    this.safeAreaTop = 0.0
-  });
-
-  final double safeAreaTop;
-  final List iconsList;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 920.h,
-      padding: EdgeInsets.fromLTRB(40.w, 80.w, 40.w, 40.w),
-      color: AppTheme.myLogoBg,
-      child: Column(
-        children: [
-        //  const TopNavBarWidget(),
-         SizedBox(height: 40.h + safeAreaTop),
-         const MyAvatarWidget(),
-          MyLoginBtnWidget(onTap: (){
-            print('登录');
-            Navigator.push( 
-              context,
-              MaterialPageRoute(builder: (context) {
-                return const LoginView();
-              }),
-            );
-          }),
-          MyActionsWidget(iconsList: iconsList)
-        ],
-      ),
     );
   }
 }
--- lib/widgets/my_widget.dart
+++ lib/widgets/my_widget.dart
@@ -1,7 +1,41 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:flutter_music_player_app/model/banners_model.dart';
 import 'package:flutter_music_player_app/theme/app_theme.dart';
+import 'package:flutter_music_player_app/views/login/login.dart';
+import 'package:flutter_music_player_app/model/user_detail_model.dart';
+import 'package:flutter_music_player_app/views/my/song_detail_view.dart';
+import 'package:flutter_music_player_app/model/user_playlist_model.dart';
+import 'package:flutter_music_player_app/theme/my_style.dart';
+
+
+
+/*
+ * 顶部自定义导航
+ */
+class TopNavBarWidget extends StatelessWidget {
+  const TopNavBarWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        const Icon(Icons.menu,color: AppTheme.allWhite),
+        SizedBox(
+          child: Row(
+            children: [
+              IconButton(onPressed: (){}, icon: const Icon(Icons.search,color: AppTheme.allWhite)),
+              IconButton(onPressed: (){}, icon: const Icon(Icons.more_vert_outlined,color: AppTheme.allWhite))
+            ],
+          ),
+        )
+      ],
+    );
+  }
+}
+
 
 /*
  * 我的头像
@@ -35,7 +69,9 @@ class MyAvatarWidget extends StatelessWidget {
   }
 }
 
-// 登录按钮
+/*
+ * 登录按钮
+ */
 class MyLoginBtnWidget extends StatelessWidget {
   final void Function()? onTap;
   const MyLoginBtnWidget({
@@ -59,7 +95,10 @@ class MyLoginBtnWidget extends StatelessWidget {
   }
 }
 
-// 功能按钮
+
+/*
+ * 功能按钮
+ */
 class MyActionsWidget extends StatelessWidget {
   const MyActionsWidget({
     super.key,
@@ -91,6 +130,226 @@ class MyActionsWidget extends StatelessWidget {
           );
         }).toList()
       ),
+    );
+  }
+}
+
+
+/*
+ * 未登录时上面部分
+ */
+class MyNotLoginTopWidget extends StatelessWidget {
+  const MyNotLoginTopWidget({
+    super.key,
+    required this.iconsList,
+    this.safeAreaTop = 0.0
+  });
+
+  final double safeAreaTop;
+  final List iconsList;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 920.h,
+      padding: EdgeInsets.fromLTRB(40.w, 80.w, 40.w, 40.w),
+      color: AppTheme.myLogoBg,
+      child: Column(
+        children: [
+         SizedBox(height: 40.h + safeAreaTop),
+         const MyAvatarWidget(),
+          MyLoginBtnWidget(onTap: (){
+            print('登录');
+            Navigator.push( 
+              context,
+              MaterialPageRoute(builder: (context) {
+                return const LoginView();
+              }),
+            );
+          }),
+          MyActionsWidget(iconsList: iconsList)
+        ],
+      ),
+    );
+  }
+}
+
+
+/*
+ * 未登录时底下部分
+ */
+class MyNotLoginBottomWidget extends StatelessWidget {
+  const MyNotLoginBottomWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text('音乐', style: TextStyle(fontSize: 48.sp, fontWeight: FontWeight.bold)),
+        SizedBox(height: 30.h),
+        Text('近期', style: TextStyle(fontSize: 40.sp, fontWeight: FontWeight.bold)),
+        SizedBox(height: 30.h),
+        Row(
+          children: [
+            Container(
+              width: 180.w,
+              height: 180.w,
+              decoration: BoxDecoration(
+                color: AppTheme.myLoveSing,
+                borderRadius: BorderRadius.circular(8)
+              ),
+              child: const Icon(Icons.heart_broken, color: AppTheme.allWhite, size: 48),
+            ),
+            SizedBox(width: 20.w),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('我喜欢的音乐', style: TextStyle(fontSize: 40.sp, fontWeight: FontWeight.bold, color: const Color.fromRGBO(67,70,84,1))),
+                Text('0首', style: TextStyle(fontSize: 35.sp, fontWeight: FontWeight.bold, color: const Color.fromRGBO(135,137,147,1)))
+              ],
+            )
+          ],
+        )
+      ],
+    );
+  }
+}
+
+
+// 登录后的顶部
+class MyLoginTopWidget extends StatelessWidget {
+  const MyLoginTopWidget({
+    super.key,
+    required this.userDetailData,
+    required this.iconsList,
+  });
+
+  final UserDetailModel userDetailData;
+  final List iconsList;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 1200.h,
+      padding: EdgeInsets.fromLTRB(40.w, 240.w, 40.w, 40.w),
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          fit: BoxFit.cover,
+          opacity: 0.8,
+          colorFilter: const ColorFilter.mode(Colors.black, BlendMode.colorDodge), // color
+          image: NetworkImage('${userDetailData.profile?.backgroundUrl}')
+        )
+      ),
+      child: Column(
+        children: [
+          // const TopNavBarWidget(),
+          SizedBox(height: 40.h),
+          MyAvatarWidget(avatar: userDetailData.profile?.avatarUrl),
+          SizedBox(height: 40.h),
+          Text('${userDetailData.profile?.nickname }', style: nicknameTextStyle),
+          SizedBox(height: 20.h),
+          Text('${userDetailData.profile?.signature}', style: signatureTextStyle),
+          SizedBox(height: 30.h),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Row(
+                children: [
+                  Text('${userDetailData.profile?.newFollows}', style: newFollowsTextStyle),
+                  SizedBox(width: 20.w),
+                  Text('关注', style: newFollowsSubTextStyle),
+                ],
+              ),
+              Row(
+                children: [
+                  Text('${userDetailData.profile?.followeds}', style: newFollowsTextStyle),
+                  SizedBox(width: 20.w),
+                  Text('粉丝', style: newFollowsSubTextStyle),
+                ],
+              ),
+              Row(
+                children: [
+                  Text('Lv.${userDetailData.level}', style: newFollowsTextStyle),
+                  SizedBox(width: 20.w),
+                  Text('等级', style: newFollowsSubTextStyle),
+                ],
+              ),
+            ],
+          ),
+          SizedBox(height: 30.h),
+          MyActionsWidget(iconsList: iconsList)
+        ],
+      ),
+    );
+  }
+}
+
+// 登录后展示歌单
+class MyLoginBottomWidget extends StatelessWidget {
+  const MyLoginBottomWidget({
+    super.key,
+    required this.userPlaylistData,
+    required this.userDetailData
+  });
+
+  final UserDetailModel userDetailData;
+  final UserPlaylistModel userPlaylistData;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children:  [
+        ListTile(
+          title: Text('音乐', style: TextStyle(fontSize: 48.sp, fontWeight: FontWeight.bold)),
+        ),
+        ListTile(
+          title: Text('近期', style:  TextStyle(fontSize: 40.sp, fontWeight: FontWeight.bold)),
+        ),
+        ...userPlaylistData.playlist!.map((e) {
+          return Padding(
+            padding: EdgeInsets.only(top: 20.h, bottom: 20.h),
+            child: InkWell(
+              onTap: (){
+                Navigator.push( 
+                  context,
+                  MaterialPageRoute(builder: (context) {
+                    return SongDetailView(
+                      id: e.id!,
+                      coverImgUrl: e.coverImgUrl!,
+                      name: e.name!,
+                      nickname: e.creator?.nickname,
+                      avatar: userDetailData.profile?.avatarUrl
+                    );
+                  }),
+                );
+              },
+              child: ListTile(
+                leading: ClipRRect(
+                  borderRadius: BorderRadius.circular(12),
+                  child: Image.network("${e.coverImgUrl}")
+                ),
+                title: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('${e.name}', overflow: TextOverflow.ellipsis, style:  playlistNameTextStyle),
+                    SizedBox(height: 15.h),
+                    Row(
+                      children: [
+                        Text('歌单 ·', style: playlistNameSubTextStyle),
+                        Text('${e.trackCount}首 ·', style: playlistNameSubTextStyle),
+                        Text('${e.creator?.nickname}', style: playlistNameSubTextStyle),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ),
+          );
+        }).toList(),
+      ],
     );
   }
 }
