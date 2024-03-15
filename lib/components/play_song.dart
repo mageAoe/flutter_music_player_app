e2eda64711459c73193d997840856d6a80e7b896
sunchaoqun <scq1144282310@163.com>
2024-03-15 11:37:09 +0800

    :sparkles: 音乐相关

---
+++ lib/components/play_song.dart
@@ -0,0 +1,16 @@
+// 音乐播放按钮组件封装
+import 'package:flutter/material.dart';
+
+class PlaySongIconBtn extends StatefulWidget {
+  const PlaySongIconBtn({super.key});
+
+  @override
+  State<PlaySongIconBtn> createState() => _PlaySongIconBtnState();
+}
+
+class _PlaySongIconBtnState extends State<PlaySongIconBtn> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/utlis/song_util.dart
@@ -0,0 +1,6 @@
+class SongUtil{
+
+  static String getSongUrl(int id) {
+    return "https://music.163.com/song/media/outer/url?id=$id.mp3";
+  }
+}
