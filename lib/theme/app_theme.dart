36376d087f5a765a68fd78ecc9a5f0c727b8b6e0
sunchaoqun <scq1144282310@163.com>
2024-04-03 17:17:55 +0800

    :sparkles: 主题

--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -64,3 +64,16 @@ class AppTheme {
 ThemeData appMusicTheme = ThemeData(
   colorScheme: ColorScheme.fromSeed(seedColor: AppTheme.myLogoBgBtn),
 );
+
+
+class HexColor extends Color {
+  HexColor(final String hexColor) : super(_getColorFromHex(hexColor));
+
+  static int _getColorFromHex(String hexColor) {
+    hexColor = hexColor.toUpperCase().replaceAll('#', '');
+    if (hexColor.length == 6) {
+      hexColor = 'FF$hexColor';
+    }
+    return int.parse(hexColor, radix: 16);
+  }
+}
