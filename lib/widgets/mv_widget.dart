42686cd741cbbe4c9dab269dd97d4dcab8f851b7
sunchaoqun <scq1144282310@163.com>
2024-04-12 14:51:09 +0800

    feat: 🚀 update mv widget

--- lib/widgets/mv_widget.dart
+++ lib/widgets/mv_widget.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_music_player_app/model/mv_exclusive_rcmd_model.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_music_player_app/model/mv_exclusive_rcmd_model.dart';
+// import 'package:flutter_music_player_app/views/mv/mv_play.dart';
+import 'package:flutter_music_player_app/views/mv/mv_play_test.dart';
 
 
 
@@ -25,38 +27,47 @@ class SinglesSectionWidget extends StatelessWidget {
       shape: const RoundedRectangleBorder(
         borderRadius: BorderRadius.all(Radius.circular(10)),
       ),
-      child: Row(
-        children: [
-          Expanded(
-            flex: 1,
-            child: ClipRRect(
-              borderRadius: const BorderRadius.all(Radius.circular(10)),
-              // child: Image.network('${topPlay.coverImgUrl}', fit: BoxFit.cover),
-              child: FadeInImage.assetNetwork(
-                height: 280.h,
-                fit: BoxFit.fill,
-                placeholder: 'assets/images/placeholder.gif',
-                image: mvData.cover!,
+      child: OnClickWidget(
+        onTap: (){
+          Navigator.push(
+            context,
+            MaterialPageRoute<void>(
+              builder: (BuildContext context) => MvPlayView(playId: mvData.id!),
+            ),
+          );
+        },
+        child: Row(
+          children: [
+            Expanded(
+              flex: 1,
+              child: ClipRRect(
+                borderRadius: const BorderRadius.all(Radius.circular(10)),
+                child: FadeInImage.assetNetwork(
+                  height: 300.h,
+                  fit: BoxFit.fill,
+                  placeholder: 'assets/images/placeholder.gif',
+                  image: mvData.cover!,
+                ),
               ),
             ),
-          ),
-          Expanded(
-            flex: 1,
-            child: Padding(
-              padding: EdgeInsets.only(left: 20.w),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(mvData.name!, maxLines: 2, overflow: TextOverflow.ellipsis),
-                  SizedBox(height: 10.h),
-                  Text(mvData.artistName!, style: TextStyle(color: Colors.teal, fontSize: 32.sp)),
-                  SizedBox(height: 10.h),
-                  Text('${getPlayCount(mvData.playCount)}播放', style: TextStyle(color: Colors.pink[400], fontSize: 32.sp)),
-                ],
+            Expanded(
+              flex: 1,
+              child: Padding(
+                padding: EdgeInsets.only(left: 20.w),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(mvData.name!, maxLines: 2, overflow: TextOverflow.ellipsis),
+                    SizedBox(height: 10.h),
+                    Text(mvData.artistName!, style: TextStyle(color: Colors.teal, fontSize: 32.sp)),
+                    SizedBox(height: 10.h),
+                    Text('${getPlayCount(mvData.playCount)}播放', style: TextStyle(color: Colors.pink[400], fontSize: 32.sp)),
+                  ],
+                ),
               ),
-            ),
-          )
-        ],
+            )
+          ],
+        ),
       ),
     );
   }
@@ -81,10 +92,10 @@ class _OnClickWidgetState extends State<OnClickWidget> {
   Widget build(BuildContext context) {
     return Ink(
       padding: EdgeInsets.zero,
-      decoration: BoxDecoration(
-        color: Colors.red,
-        borderRadius: BorderRadius.circular(25),
-      ),
+      // decoration: BoxDecoration(
+      //   color: Colors.red,
+      //   borderRadius: BorderRadius.circular(25),
+      // ),
       child: InkResponse(
         /// 是否需要按需剪裁水波纹
         containedInkWell: true,
@@ -93,7 +104,7 @@ class _OnClickWidgetState extends State<OnClickWidget> {
         /// 点击时，背景效果的形状（rectangle 或 circle）
         highlightShape: BoxShape.rectangle,
         /// 点击时，背景效果的颜色
-        highlightColor: Colors.blue,
+        highlightColor: Colors.grey[100],
         onTap: widget.onTap,
         child: widget.child,
       ),
